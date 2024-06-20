a2145e8cf2ffdf26eb29de65fe73150a4430efcf
EddyG <eddyg_2d@mail.ru>
2024-06-20 14:52:31 +0300

    Initial commit

--- src/main/kotlin/Commission.kt
+++ src/main/kotlin/Commission.kt
@@ -29,29 +29,30 @@ fun calculationCommission(typeCard: String, previousSum: Int = 0, amount: Int,
                           percentCommissionMasterCard: Double = 0.006, minCommissionMasterCard: Int = 20,
                           percentCommissionVisa: Double = 0.0075, minCommissionVisa: Int = 35,
                           maxMonthLimitMasterCard: Int = 75_000, maxDayLimitCard: Int = 150_000,
-                          maxMonthLimitCard: Int = 600_000) {
+                          maxMonthLimitCard: Int = 600_000): Int {
     var sumTransfer = 0
     println(typeCard)
 
-       if (amount + previousSum <= maxMonthLimitCard) {
-            if (amount + previousSum <= maxDayLimitCard) {
-                if (typeCard == "MIR") {
-                    sumTransfer = amount
-                    println("Сумма комиссии: 0 руб.")
-                    println("Сумма перевода: $sumTransfer")
-                }
+    if (amount + previousSum <= maxMonthLimitCard) {
+        if (amount + previousSum <= maxDayLimitCard) {
+            if (typeCard == "MIR") {
+                sumTransfer = amount
+                println("Сумма комиссии: 0 руб.")
+                println("Сумма перевода: $sumTransfer руб.")
+            }
 
-                if (typeCard == "VISA") {
-                    if ((amount * percentCommissionVisa) < minCommissionVisa) {
+
+            if (typeCard == "VISA") {
+                if ((amount * percentCommissionVisa) < minCommissionVisa) {
                     sumTransfer = (amount - minCommissionVisa)
-                    println("Сумма комиссии: $minCommissionVisa")
+                    println("Сумма комиссии: $minCommissionVisa руб.")
 
                 } else {
                     sumTransfer = (amount - (amount * percentCommissionVisa)).toInt()
-                    println("Сумма комиссии: " + (amount * percentCommissionVisa))
+                    println("Сумма комиссии: " + (amount * percentCommissionVisa) + " руб.")
                 }
 
-                println("Сумма перевода: $sumTransfer")
+                println("Сумма перевода: $sumTransfer руб.")
             }
 
             if (typeCard == "MasterCard") {
@@ -62,23 +63,25 @@ fun calculationCommission(typeCard: String, previousSum: Int = 0, amount: Int,
 
                 if (previousSum > maxMonthLimitMasterCard) {
                     sumTransfer = (amount - (amount * percentCommissionMasterCard) - minCommissionMasterCard).toInt()
-                    println("Сумма комиссии: " + ((amount * percentCommissionMasterCard) + minCommissionMasterCard))
+                    println("Сумма комиссии: " + ((amount * percentCommissionMasterCard) + minCommissionMasterCard) + " руб.")
                 }
                 if ((amount + previousSum) > maxMonthLimitMasterCard && previousSum <= maxMonthLimitMasterCard) {
                     sumTransfer = ((amount - (((amount + previousSum) - maxMonthLimitMasterCard)
                             * percentCommissionMasterCard)) - minCommissionMasterCard).toInt()
-                    println("Сумма комиссии: " + ((((amount + previousSum) - maxMonthLimitMasterCard)
-                            * percentCommissionMasterCard) + minCommissionMasterCard))
+                    println(
+                        "Сумма комиссии: " + ((((amount + previousSum) - maxMonthLimitMasterCard)
+                                * percentCommissionMasterCard) + minCommissionMasterCard) + " руб."
+                    )
                 }
-                println("Сумма перевода: $sumTransfer")
+                println("Сумма перевода: $sumTransfer руб.")
             }
 
-            } else {
+        } else {
             println("Превышен лимит перевода по карте за сутки")
         }
 
-       } else {
+    } else {
         println("Превышен лимит перевода по карте за месяц")
     }
-
+    return sumTransfer
 }
--- src/test/kotlin/CommissionKtTest.kt
+++ src/test/kotlin/CommissionKtTest.kt
@@ -5,30 +5,149 @@ import org.junit.Assert.*
 class CommissionKtTest {
 
     @Test
-    fun calculationCommission() {
-        var typeCard = "MIR"
-        var sumTransfer = 0
-        val expectedResult = 0
+    fun calculationCommissionMasterCard() {
+        val typeCard = "MasterCard"
+        val previousSum = 0
+        val percentCommissionMasterCard = 0.006
+        val minCommissionMasterCard = 20
+        val percentCommissionVisa = 0.0075
+        val minCommissionVisa = 35
+        val maxMonthLimitMasterCard = 75_000
+        val maxDayLimitCard = 150_000
+        val maxMonthLimitCard = 600_000
+        val expectedResult = 79_950
+        val amount = 80_000
 
-        print("Введите сумму перевода в рублях: ")
-        val amount: Int = readln().toInt()
-
-        print("Введите номер (первую цифру) карты для перевода: ")
-        val numberCard = readln().toCharArray()
-        val symbolCard = (numberCard.first())
-        when (symbolCard.toString()) {
-            "2" -> typeCard = "MIR"
-            "4" -> typeCard = "VISA"
-            "5" -> typeCard = "MasterCard"
-        }
-        val result =  calculationCommission(typeCard, previousSum = 0, amount,
-            percentCommissionMasterCard = 0.6, minCommissionMasterCard = 20,
-            percentCommissionVisa = 0.75, minCommissionVisa = 35,
-            maxMonthLimitMasterCard = 75_000, maxDayLimitCard = 150_000,
-            maxMonthLimitCard = 600_000)
+        val result = calculationCommission(typeCard, previousSum, amount, percentCommissionMasterCard,
+            minCommissionMasterCard, percentCommissionVisa, minCommissionVisa, maxMonthLimitMasterCard,
+            maxDayLimitCard, maxMonthLimitCard)
 
         assertEquals(expectedResult, result)
+    }
 
+    @Test
+    fun calculationCommissionMaxMonthlyLimitMasterCard() {
+        val typeCard = "MasterCard"
+        val previousSum = 50_000
+        val percentCommissionMasterCard = 0.006
+        val minCommissionMasterCard = 20
+        val percentCommissionVisa = 0.0075
+        val minCommissionVisa = 35
+        val maxMonthLimitMasterCard = 75_000
+        val maxDayLimitCard = 150_000
+        val maxMonthLimitCard = 600_000
+        val expectedResult = 25_000
+        val amount = 25_000
+
+        val result = calculationCommission(typeCard, previousSum, amount, percentCommissionMasterCard,
+            minCommissionMasterCard, percentCommissionVisa, minCommissionVisa, maxMonthLimitMasterCard,
+            maxDayLimitCard, maxMonthLimitCard)
+
+        assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun calculationCommissionMIR() {
+        val typeCard = "MIR"
+        val previousSum = 0
+        val percentCommissionMasterCard = 0.006
+        val minCommissionMasterCard = 20
+        val percentCommissionVisa = 0.0075
+        val minCommissionVisa = 35
+        val maxMonthLimitMasterCard = 75_000
+        val maxDayLimitCard = 150_000
+        val maxMonthLimitCard = 600_000
+        val expectedResult = 50_000
+        val amount = 50_000
+
+        val result = calculationCommission(typeCard, previousSum, amount, percentCommissionMasterCard,
+            minCommissionMasterCard, percentCommissionVisa, minCommissionVisa, maxMonthLimitMasterCard,
+            maxDayLimitCard, maxMonthLimitCard)
+
+        assertEquals(expectedResult, result)
+    }
+    @Test
+    fun calculationCommissionVisa() {
+        val typeCard = "VISA"
+        val previousSum = 0
+        val percentCommissionMasterCard = 0.006
+        val minCommissionMasterCard = 20
+        val percentCommissionVisa = 0.0075
+        val minCommissionVisa = 35
+        val maxMonthLimitMasterCard = 75_000
+        val maxDayLimitCard = 150_000
+        val maxMonthLimitCard = 600_000
+        val expectedResult = 9_925
+        val amount = 10_000
+
+        val result = calculationCommission(typeCard, previousSum, amount, percentCommissionMasterCard,
+            minCommissionMasterCard, percentCommissionVisa, minCommissionVisa, maxMonthLimitMasterCard,
+            maxDayLimitCard, maxMonthLimitCard)
+
+        assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun calculationMinCommissionVisa() {
+        val typeCard = "VISA"
+        val previousSum = 0
+        val percentCommissionMasterCard = 0.006
+        val minCommissionMasterCard = 20
+        val percentCommissionVisa = 0.0075
+        val minCommissionVisa = 35
+        val maxMonthLimitMasterCard = 75_000
+        val maxDayLimitCard = 150_000
+        val maxMonthLimitCard = 600_000
+        val expectedResult = 965
+        val amount = 1_000
+
+        val result = calculationCommission(typeCard, previousSum, amount, percentCommissionMasterCard,
+            minCommissionMasterCard, percentCommissionVisa, minCommissionVisa, maxMonthLimitMasterCard,
+            maxDayLimitCard, maxMonthLimitCard)
+
+        assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun maxDayLimitCard() {
+        val typeCard = "MasterCard"
+        val previousSum = 90_000
+        val percentCommissionMasterCard = 0.006
+        val minCommissionMasterCard = 20
+        val percentCommissionVisa = 0.0075
+        val minCommissionVisa = 35
+        val maxMonthLimitMasterCard = 75_000
+        val maxDayLimitCard = 150_000
+        val maxMonthLimitCard = 600_000
+        val expectedResult = 0
+        val amount = 80_000
+
+        val result = calculationCommission(typeCard, previousSum, amount, percentCommissionMasterCard,
+            minCommissionMasterCard, percentCommissionVisa, minCommissionVisa, maxMonthLimitMasterCard,
+            maxDayLimitCard, maxMonthLimitCard)
+
+        assertEquals(expectedResult, result)
+    }
+    
+    @Test
+    fun maxMonthlyLimitCard() {
+        val typeCard = "MasterCard"
+        val previousSum = 500_000
+        val percentCommissionMasterCard = 0.006
+        val minCommissionMasterCard = 20
+        val percentCommissionVisa = 0.0075
+        val minCommissionVisa = 35
+        val maxMonthLimitMasterCard = 75_000
+        val maxDayLimitCard = 150_000
+        val maxMonthLimitCard = 600_000
+        val expectedResult = 0
+        val amount = 180_000
+
+        val result = calculationCommission(typeCard, previousSum, amount, percentCommissionMasterCard,
+            minCommissionMasterCard, percentCommissionVisa, minCommissionVisa, maxMonthLimitMasterCard,
+            maxDayLimitCard, maxMonthLimitCard)
+
+        assertEquals(expectedResult, result)
     }
 }
 
